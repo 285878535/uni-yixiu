151555480c196d356443bf7926750a1e470ca0ed
mac <mac@macs-MacBook-Pro.local>
2025-11-20 15:15:50 +0800

    补全代码

---
+++ pages/404/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const router = useRouter()
+const { t } = useI18n()
+const goHome = () => {
+  router.replace("/")
+}
+</script>
+
+<template>
+  <div flex justify-center items-center class="h100%">
+    <div flex flex-col items-center>
+      <img w="full" src="@/assets/images/404.png" alt="404" />
+      <van-button type="primary" @click="goHome">{{
+        t("backHomeBtn")
+      }}</van-button>
+    </div>
+  </div>
+</template>
